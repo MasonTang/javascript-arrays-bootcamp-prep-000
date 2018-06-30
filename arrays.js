25a8f3e6ef80481f12f33281a536cd7fbc40ce0c
masontang@aol.com <masontang@aol.com>
2018-06-30 02:40:38 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -52,7 +52,7 @@ function destructivelyRemoveElementFromEndOfArray(array){
 
 destructivelyRemoveElementFromEndOfArray(array)
 
-function(removeElementFromEndOfArray){
+function removeElementFromEndOfArray(array){
   return array.slice(-1)
 }
 
